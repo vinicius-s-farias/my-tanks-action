f55ca306409cabf7ec1f82c5ed20c85eb4d97dab
vinicius-s-farias <vifarias47@gmail.com>
2021-12-22 10:11:54 -0300

    por favor Deus

--- src/action.js
+++ src/action.js
@@ -7,7 +7,7 @@ async function run(){
   const TENOR_TOKEN = core.getInput('TENOR_TOKEN');
 
   const randomPos = Math.round(Math.random() * 1000);
-  const url = https://api.tenor.com/v1/search?q=thank%20you&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN};
+  const url = `https://api.tenor.com/v1/search?q=thank%20you&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
   const response = await fetch(url);
   const { results } = await response.json();
   const gifUrl = results[0].media[0].tinygif.url;
