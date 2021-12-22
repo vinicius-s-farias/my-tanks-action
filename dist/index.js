413af95324bcff88f1743f9991a3be8d5e106183
vinicius-s-farias <vifarias47@gmail.com>
2021-12-22 08:53:54 -0300

    correcao

--- dist/index.js
+++ dist/index.js
@@ -8448,7 +8448,7 @@ const github = __nccwpck_require__(5438);
 async function run(){
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
 
-    const octokit = github.getOctokit('GITHUB_TOKEN');
+    const octokit = github.getOctokit(GITHUB_TOKEN);
 
     const { context = {} } = github;
     const { pull_request } = context.payload;
--- src/action.js
+++ src/action.js
@@ -4,7 +4,7 @@ const github = require('@actions/github');
 async function run(){
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
 
-    const octokit = github.getOctokit('GITHUB_TOKEN');
+    const octokit = github.getOctokit(GITHUB_TOKEN);
 
     const { context = {} } = github;
     const { pull_request } = context.payload;
