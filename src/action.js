dc1e6e2b673ec0b92183e9006aeb84a87538f7ad
vinicius-s-farias <vifarias47@gmail.com>
2021-12-22 08:41:56 -0300

    sepa vai

--- src/action.js
+++ src/action.js
@@ -9,10 +9,10 @@ async function run(){
     const { context = {} } = github;
     const { pull_request } = context.payload;
 
-    await octokit.issues.creatComment({
-        ...context.repo,
-        issue_number: pull_request.number,
-        body: 'Thank you for submitting a pull request !! We will try to review this as soon as we can !!'
+    await octokit.rest.issues.creatComment({
+      ...context.repo,
+      issue_number: pull_request.number,
+      body: 'Thank you for submitting a pull request !! We will try to review this as soon as we can !!'
 
     });
 }
